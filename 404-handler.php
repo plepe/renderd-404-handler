a330894ef28f5f141e3ac1d0d3864471388df216
Stephan Bösch-Plepelits <skunk@xover.mud.at>
2015-03-14 06:41:02 +0100

    Read renderd config file

--- 404-handler.php
+++ 404-handler.php
@@ -1,6 +1,36 @@
 <?php include "conf.php"; /* load a local configuration */ ?>
-<?
+<?php
 Header("HTTP/1.1 404 Not Found");
+
+function renderd_conf_parse($file) {
+  $block = null;
+  $ret = array();
+
+  $f = fopen($file, "r");
+
+  while($r = fgets($f)) {
+    chop($r);
+
+    // comment
+    if(preg_match("/^\s*#/", $r, $m))
+      ;
+
+    // new block opener
+    elseif(preg_match("/\[(.*)\]/", $r, $m)) {
+      $block = $m[1];
+      $ret[$block] = array();
+    }
+
+    elseif($block && preg_match("/^([^=]*)=([^#]*)/", $r, $m))
+      $ret[$block][trim($m[1])] = trim($m[2]);
+  }
+
+  fclose($f);
+
+  return $ret;
+}
+
+$renderd_conf = renderd_conf_parse($renderd_conf_file);
 
 $list=array(array(10, array(
   'body'=>"The requested URL {$_SERVER['REQUEST_URI']} was not found on this server.",
